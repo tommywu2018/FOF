01368575989218eb3d2ca23221146818c128641d
wangbin750 <wangbin750@126.com>
2017-02-14 22:09:29 +0800

    修改

--- Inspector/Inspector.py
+++ Inspector/Inspector.py
@@ -8,7 +8,7 @@ from pylab import *
 import datetime as dt
 from WindPy import *
 import xlrd
-import xlwt
+from xlwt import Workbook, easyxf
 from xlutils.copy import copy
 w.start()
 
@@ -20,33 +20,41 @@ def Wsd_Data_Install(code, fields, startdate, enddate):
         data = pd.DataFrame(np.array(temp_data.Data).T, index=temp_data.Times, columns=temp_data.Fields)
         return data
     else:
-        return "ErrodCode=%s" % temp_data.ErrorCode
+        print "ErrodCode=%s" % temp_data.ErrorCode
+        raise Exception("error in data install!")
 
 # 获取日内数据
-def Wsi_Data_Install(code, startdate, enddate):
-    temp_data = w.wsi(code, "close", startdate, enddate, "BarSize=5")
+# interval-周期，以分钟计
+def Wsi_Data_Install(code, startdate, enddate, interval):
+    temp_data = w.wsi(code, "close", startdate, enddate, "BarSize=%s"%interval)
     if temp_data.ErrorCode == 0:
         data = pd.DataFrame(np.array(temp_data.Data).T, index=temp_data.Times, columns=temp_data.Fields)
-    return data
+        return data
+    else:
+        return "no wsi data"
 
 # 计算日内波动率
-def Intraday_Volatility(code, startdate, enddate):
+def Intraday_Volatility(code, startdate, enddate, interval):
     enddate = dt.datetime.strptime(enddate, "%Y-%m-%d") + timedelta(1)
     enddate = enddate.strftime("%Y-%m-%d")
     date_set = pd.date_range(startdate, enddate, freq="D")
-    data = Wsi_Data_Install(code, startdate, enddate)
-    intraday_std_list = list()
-    date_list = list()
-    for each in range(len(date_set) - 1):
-        data_temp = data.loc[date_set[each]:date_set[each+1]]
-        if data_temp.empty:
-            pass
-        else:
-            intraday_std = data_temp.pct_change().std()
-            intraday_std_list.append(intraday_std)
-            date_list.append(date_set[each])
-    intraday_std_frame = pd.DataFrame(np.array(intraday_std_list), index=date_list, columns=["intraday_std"])
-    return intraday_std_frame
+    data = Wsi_Data_Install(code, startdate, enddate, interval)
+    print (data is "no wsi data")
+    if (data is "no wsi data"):
+        return pd.DataFrame(columns=["intraday_std"])
+    else:
+        intraday_std_list = list()
+        date_list = list()
+        for each in range(len(date_set) - 1):
+            data_temp = data.loc[date_set[each]:date_set[each+1]]
+            if data_temp.empty:
+                pass
+            else:
+                intraday_std = 100 * (data_temp.pct_change().std() * ((240.0/interval)**0.5))
+                intraday_std_list.append(intraday_std)
+                date_list.append(date_set[each])
+        intraday_std_frame = pd.DataFrame(np.array(intraday_std_list), index=date_list, columns=["intraday_std"])
+        return intraday_std_frame
 
 test = pd.DataFrame(np.random.randn(100))
 test.std()
@@ -59,7 +67,7 @@ def Portfolio_Net_Value(code_list, weight_list, previous_nv, today_date):
         temp_data = w.wsd(each_code, "NAV_adj", yesterday_date, today_date)
         if temp_data.ErrorCode != 0:
             print temp_data.ErrorCode
-            break
+            raise Exception("error in data install!")
         else:
             pct_chg = (temp_data.Data[0][1] - temp_data.Data[0][0])/temp_data.Data[0][0]
             pct_chg_list.append(temp_data.Data[0][0])
@@ -69,18 +77,27 @@ def Portfolio_Net_Value(code_list, weight_list, previous_nv, today_date):
         nv_today = previous_nv * (1 + nv_pct_chg)
         return nv_today
     else:
-        return "Error"
+        raise Exception("missing data!")
 
-def Index_ETF_Performance(code, startdate, enddate):
+def Index_ETF_Performance(code, startdate, enddate, interval):
     wsd_data = Wsd_Data_Install(code, ["pct_chg", "swing", "amt", "free_turn"], startdate, enddate)
     today_data = list(wsd_data.iloc[-1])
     upper_quantile = list(wsd_data.iloc[:-1].quantile(0.9))
     lower_quantile = list(wsd_data.iloc[:-1].quantile(0.1))
-    intraday_std_data = Intraday_Volatility(code, startdate, enddate)
+    intraday_std_data = Intraday_Volatility(code, startdate, enddate, interval)
     today_data.append(intraday_std_data.iloc[-1,0])
     upper_quantile.append(intraday_std_data.iloc[0:-1,0].quantile(0.9))
     lower_quantile.append(intraday_std_data.iloc[0:-1,0].quantile(0.1))
     data = pd.DataFrame(np.array([today_data, upper_quantile, lower_quantile]), index=[u"今日值", u"上10%分位数", u"下10%分位数"], columns=[u"涨跌幅", u"振幅", u"成交额", u"换手率", u"日内波动率"])
+    return data
+
+def Bondindex_Performance(code, startdate, enddate):
+    startdate = str(w.tdaysoffset(-1, startdate).Data[0][0])[:10]
+    wsd_data = Wsd_Data_Install(code, ["pct_chg", "amt"], startdate, enddate)
+    today_data = list(wsd_data.iloc[-1])
+    upper_quantile = list(wsd_data.iloc[:-1].quantile(0.9))
+    lower_quantile = list(wsd_data.iloc[:-1].quantile(0.1))
+    data = pd.DataFrame(np.array([today_data, upper_quantile, lower_quantile]), index=[u"今日值", u"上10%分位数", u"下10%分位数"], columns=[u"涨跌幅", u"成交额"])
     return data
 
 def Publicfund_Performance(code, startdate, enddate):
@@ -92,32 +109,93 @@ def Publicfund_Performance(code, startdate, enddate):
     data = pd.DataFrame(np.array([today_data, upper_quantile, lower_quantile]), index=[u"今日值", u"上10%分位数", u"下10%分位数"], columns=[u"净值涨跌幅"])
     return data
 
+def Xls_Writer(ws, data, rowno, colno, field):
+    if field in [u"涨跌幅", u"振幅", u"日内波动率", u"换手率"]:
+        if data[u"今日值"] < data[u"下10%分位数"]:
+            ws.write_merge(rowno, rowno, colno, colno+1, "%0.3f%%"%data[u"今日值"], style_low)
+        elif data[u"今日值"] > data[u"上10%分位数"]:
+            ws.write_merge(rowno, rowno, colno, colno+1, "%0.3f%%"%data[u"今日值"], style_high)
+        else:
+            ws.write_merge(rowno, rowno, colno, colno+1, "%0.3f%%"%data[u"今日值"], style_normal)
+        ws.write(rowno+1, colno, "%0.3f%%"%data[u"下10%分位数"], style_quan)
+        ws.write(rowno+1, colno+1, "%0.3f%%"%data[u"上10%分位数"], style_quan)
+    elif field in [u"成交额"]:
+        if data[u"今日值"] < data[u"下10%分位数"]:
+            ws.write_merge(rowno, rowno, colno, colno+1, "%0.2f"%(data[u"今日值"]/100000000), style_low)
+        elif data[u"今日值"] > data[u"上10%分位数"]:
+            ws.write_merge(rowno, rowno, colno, colno+1, "%0.2f"%(data[u"今日值"]/100000000), style_high)
+        else:
+            ws.write_merge(rowno, rowno, colno, colno+1, "%0.2f"%(data[u"今日值"]/100000000), style_normal)
+        ws.write(rowno+1, colno, "%0.2f"%(data[u"下10%分位数"]/100000000), style_quan)
+        ws.write(rowno+1, colno+1, "%0.2f"%(data[u"上10%分位数"]/100000000), style_quan)
+    else:
+        raise Exception("not defined field!")
 
-Index_Performance("000300.SH", "2016-11-11", "2017-02-10")
-code = "000300.SH"
-startdate = "2016-11-11"
-enddate = "2017-02-10"
-wsd_data = w.wsd(code, ["pct_chg", "swing", "amt", "free_turn"], startdate, enddate)
-print wsd_data
-wsd_data.iloc[:-1,0].quantile(0.1)
 '''
 close:收盘价
 amt:交易量
 pct_chg:涨跌幅
 swing:振幅
 free_turn:换手率（以自由流通股本计）
-NAV_adj:
-NAV_acc:
-
+NAV_adj:复权净值
+NAV_acc:累计净值
 '''
+
 day_shift = 120
 week_shift = 52
 month_shift = 36
 
 today_date = dt.date.today().isoformat()
+today_date = "2017-02-13"
 #print today_date
 
 daily_backtest_start_date = str(w.tdaysoffset(-day_shift, today_date).Data[0][0])[:10]
 #print daily_backtest_start_date
 
-pd.Series(np.random.randn(8)).quantile(0.5)
+'''
+格式定义
+'''
+border_style = "border: left thin, right thin, top thin, bottom thin;"
+style_normal = easyxf("font: colour white;" "pattern: pattern solid, fore_colour green;" "align: vertical center, horizontal center;" "%s" % border_style)
+style_low = easyxf("font: colour white;" "pattern: pattern solid, fore_colour blue;" "align: vertical center, horizontal center;" "%s" % border_style)
+style_high = easyxf("font: colour white;" "pattern: pattern solid, fore_colour red;" "align: vertical center, horizontal center;" "%s" % border_style)
+style_name = easyxf("font: colour black;" "pattern: pattern solid, fore_colour white;" "align: vertical center, horizontal left;" "%s" % border_style)
+style_quan = easyxf("font: colour black;" "pattern: pattern solid, fore_colour white;" "align: vertical center, horizontal center;" "%s" % border_style)
+ws = Workbook()
+ws_index = ws.add_sheet("Index")
+
+stock_index_list = ["000300.SH", "000905.SH", "SPX.GI", "HSI.HI"]
+bond_index_list = ["H11001.CSI"]
+
+col_no = 1
+for each in [u"涨跌幅", u"振幅", u"日内波动率", u"成交额(亿元)", u"换手率"]:
+    ws_index.write_merge(0, 0, col_no, col_no+1, each, style_quan)
+    col_no = col_no + 2
+
+row_no = 1
+
+for each_index in stock_index_list:
+    print each_index
+    temp_result = Index_ETF_Performance(each_index, daily_backtest_start_date, today_date, 5)
+    ws_index.write_merge(row_no, row_no+1, 0, 0, each_index, style_name)
+    col_no = 1
+    for each_field in [u"涨跌幅", u"振幅", u"日内波动率", u"成交额", u"换手率"]:
+        temp_series = temp_result[each_field]
+        Xls_Writer(ws_index, temp_series, row_no, col_no, each_field)
+        col_no = col_no + 2
+    row_no = row_no + 2
+
+for each_index in bond_index_list:
+    temp_result = Bondindex_Performance(each_index, daily_backtest_start_date, today_date)
+    ws_index.write_merge(row_no, row_no+1, 0, 0, each_index, style_name)
+    col_no = 1
+    for each_field in [u"涨跌幅", u"振幅", u"日内波动率", u"成交额", u"换手率"]:
+        try:
+            temp_series = temp_result[each_field]
+            Xls_Writer(ws_index, temp_series, row_no, col_no, each_field)
+        except:
+            pass
+        col_no = col_no + 2
+    row_no = row_no + 2
+
+ws.save("D:\\test.xls")
