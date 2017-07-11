1f9907e19d8d899c0243601a0b8f710f5f66cd28
wangbin750 <wangbin750@126.com>
2017-07-11 16:58:20 +0800

    20170711
    CPPI

--- Robo-Advisor/test.py
+++ Robo-Advisor/test.py
@@ -1,3 +1,21 @@
+# coding=utf-8
+import codecs
+
 from Portfolio_Formation import *
 
-print Portfolio_Form(5)
+path = u'Z:\Mac 上的 WangBin-Mac\FOF\Robo-Advisor'
+
+for each in range(1,11):
+    portfolio_imformation = Portfolio_Form(each)
+    full_path = path + "\portfolio_" + str(each) + '.txt'
+    file = codecs.open(full_path, 'w', 'utf-8')
+    file.write(portfolio_imformation)
+    file.close()
+    print('Done')
+
+for each in range(1, 11):
+    full_path = path + "\portfolio_" + str(each) + '.txt'
+    file = codecs.open(full_path)
+    print file.read()
+
+full_path = '/Users/WangBin-Mac/FOF/Robo-Advisor/portfolio_1.txt'
